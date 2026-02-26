325975ce31ce0e3b5148ea868d7765170cc51be4
Lucas <lucasalves_taz@hotmail.com>
2026-02-25 21:16:32 -0300

    up app.py

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
 
 ########################
 # Configuração da Página
 ########################
-st.set_page_config(
-    page_title="FUTSTATS",
-    page_icon="⚽",
-    layout="wide"
-)
+st.set_page_config(page_title="FUTSTATS", page_icon="⚽", layout="wide")
+
 
 ########################
 # Carregar os dados
 ########################
-df = pd.read_csv('silver/Dados_Betfair_Exchange_HT00.csv')
+@st.cache_data(ttl=300)
+def load_data():
+    data = pd.read_csv("silver/Dados_Betfair_Exchange_HT00.csv")
+    return data
+
 
 ########################
 # Título da página
 ########################
-st.title("⚽ FUTSTATS APP HT - FT")
+st.title("⚽ FutStats HT - FT")
 
+########################
+# Abas da página
+########################
+tab1, tab2 = st.tabs(["Mandante | HT - FT", "Visitante | HT - FT"])
 
-st.table()
+with tab1:
+    home = st.selectbox("Selecione o time mandante:", load_data()["Home"].unique())
+
+with tab2:
+    away = st.selectbox("Selecione o time visitante:", load_data()["Away"].unique())
